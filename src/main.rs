b7d45f11c3ad6d603e35cf0453f6afa967957bff
paragonnov <qkdxorjs1002@naver.com>
2024-11-20 17:52:29 +0900

    2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let rannum: u32 = rand::thread_rng().gen_range(1..=10);
+    loop {
+        println!("Input: ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("err");
+        println!("This is interpolation, input: {input}");
+        let parsed = match input.trim().parse::<u32>() {
+            Err(_) => continue,
+            Ok(n) => n,
+        };
+
+        match parsed.cmp(&rannum) {
+            Ordering::Equal => {
+                println!("T");
+                break;
+            }
+            Ordering::Less => println!("F"),
+            Ordering::Greater => println!("F"),
+        }
+    }
 }
